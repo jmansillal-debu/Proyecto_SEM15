4f93a1607483197ae3ab7727ef232049147f39f8
jmansillal-debu <jmansillal@iestlarecoleta.edu.pe>
2026-08-25 11:34:02 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -8,16 +8,6 @@ const firebaseConfig = {
   appId: "1:926454626159:web:f1cfb4B36a810ac1a91a9a",
   measurementId: "G-V966LGLNZB"
 };
-// CONFIGURACIÓN DE FIREBASE
-const firebaseConfig = {
-  apiKey: "AIzaSyD-EXAMPLE_KEY_1234567890",
-  authDomain: "tu-proyecto.firebaseapp.com",
-  databaseURL: "https://tu-proyecto-default-rtdb.firebaseio.com",
-  projectId: "tu-proyecto",
-  storageBucket: "tu-proyecto.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
-};
 
 let db = null;
 let isFirebaseConnected = false;
